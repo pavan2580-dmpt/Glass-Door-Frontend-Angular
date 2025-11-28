f5d0b043a2d98fe0f68bd7ae6d45fa5355acfa2b
pavan2580-dmpt <pavanganesh2580@gmail.com>
2025-11-28 21:41:52 +0530

    updated the backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // apiURL: 'http://localhost:9130/api/v1/',
-    apiURL:"https://glass-door-backend.vercel.app/"
+    apiURL:"https://glass-door-backend.vercel.app/api/v1/"
   };
   
